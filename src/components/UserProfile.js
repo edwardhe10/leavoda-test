eddffbedf1ffa3a3e2e2a5dc25ab580d6d804c24
edwardhe10 <88951771+edwardhe10@users.noreply.github.com>
2025-06-24 15:28:55 -0400

    finished components

---
+++ src/components/UserProfile.js
@@ -0,0 +1,26 @@
+import { useSelector } from 'react-redux';
+import { Avatar, Card, CardContent, Typography } from '@mui/material';
+
+export default function UserProfile() {
+  const user = useSelector(state => state.user.data);
+
+  if (!user) return <p>Loading...</p>;
+
+  return (
+    <Card style={{ maxWidth: 500, margin: 'auto', marginTop: 32 }}>
+      <CardContent>
+        <Avatar src={user.image} style={{ width: 80, height: 80, margin: 'auto' }} />
+        <Typography variant="h5" align="center">{user.firstName} {user.lastName}</Typography>
+        <Typography align="center">{user.gender}, {user.age} years</Typography>
+        <Typography variant="h6" style={{ marginTop: 16 }}>Address</Typography>
+        <Typography>{user.address.address}, {user.address.city}, {user.address.state}</Typography>
+        <Typography variant="h6" style={{ marginTop: 16 }}>Work</Typography>
+        <Typography>{user.company.name} - {user.company.department} ({user.company.title})</Typography>
+        <Typography variant="h6" style={{ marginTop: 16 }}>Contact</Typography>
+        <Typography>{user.firstName} {user.lastName}</Typography>
+        <Typography>{user.phone}</Typography>
+        <Typography>{user.email}</Typography>
+      </CardContent>
+    </Card>
+  );
+}
